f42df50d61cdfc1017ed4bd0906da8e678726ee3
Cody <contact@codychu.com>
2024-05-19 21:41:24 -0400

    modified:   src/NBA_Bets.jsx 	modified:   src/NBA_Points.jsx 	modified:   src/teamImages.jsx

--- src/NBA_Bets.jsx
+++ src/NBA_Bets.jsx
@@ -1,71 +1,9 @@
 import { useState, useEffect } from 'react'
 import './NBA_Bets.css'
-import atlantaHawks from '/NBA_Teams/Atlanta Hawks.svg';
-import bostonCeltics from '/NBA_Teams/Boston Celtics.svg';
-import brooklynNets from '/NBA_Teams/Brooklyn Nets.svg';
-import charlotteHornets from '/NBA_Teams/Charlotte Hornets.svg';
-import chicagoBulls from '/NBA_Teams/Chicago Bulls.svg';
-import clevelandCavaliers from '/NBA_Teams/Cleveland Cavaliers.svg';
-import dallasMavericks from '/NBA_Teams/Dallas Mavericks.svg';
-import denverNuggets from '/NBA_Teams/Denver Nuggets.svg';
-import detroitPistons from '/NBA_Teams/Detroit Pistons.svg';
-import goldenStateWarriors from '/NBA_Teams/Golden State Warriors.svg';
-import houstonRockets from '/NBA_Teams/Houston Rockets.svg';
-import indianaPacers from '/NBA_Teams/Indiana Pacers.svg';
-import laClippers from '/NBA_Teams/Los Angeles Clippers.svg';
-import laLakers from '/NBA_Teams/Los Angeles Lakers.svg';
-import memphisGrizzlies from '/NBA_Teams/Memphis Grizzlies.svg';
-import miamiHeat from '/NBA_Teams/Miami Heat.svg';
-import milwaukeeBucks from '/NBA_Teams/Milwaukee Bucks.svg';
-import minnesotaTimberwolves from '/NBA_Teams/Minnesota Timberwolves.svg';
-import newOrleansPelicans from '/NBA_Teams/New Orleans Pelicans.svg';
-import newYorkKnicks from '/NBA_Teams/New York Knicks.svg';
-import oklahomaCityThunder from '/NBA_Teams/Oklahoma City Thunder.svg';
-import orlandoMagic from '/NBA_Teams/Orlando Magic.svg';
-import philadelphia76ers from '/NBA_Teams/Philadelphia 76ers.svg';
-import phoenixSuns from '/NBA_Teams/Phoenix Suns.svg';
-import portlandTrailBlazers from '/NBA_Teams/Portland Trail Blazers.svg';
-import sacramentoKings from '/NBA_Teams/Sacramento Kings.svg';
-import sanAntonioSpurs from '/NBA_Teams/San Antonio Spurs.svg';
-import torontoRaptors from '/NBA_Teams/Toronto Raptors.svg';
-import utahJazz from '/NBA_Teams/Utah Jazz.svg';
-import washingtonWizards from '/NBA_Teams/Washington Wizards.svg';
+import { teamImages } from './teamImages.jsx';
 
 function NBA_Bets() {
   const [data, setData] = useState(null)
-
-  const teamImages = {
-    'Atlanta Hawks': atlantaHawks,
-    'Boston Celtics': bostonCeltics,
-    'Brooklyn Nets': brooklynNets,
-    'Charlotte Hornets': charlotteHornets,
-    'Chicago Bulls': chicagoBulls,
-    'Cleveland Cavaliers': clevelandCavaliers,
-    'Dallas Mavericks': dallasMavericks,
-    'Denver Nuggets': denverNuggets,
-    'Detroit Pistons': detroitPistons,
-    'Golden State Warriors': goldenStateWarriors,
-    'Houston Rockets': houstonRockets,
-    'Indiana Pacers': indianaPacers,
-    'Los Angeles Clippers': laClippers,
-    'Los Angeles Lakers': laLakers,
-    'Memphis Grizzlies': memphisGrizzlies,
-    'Miami Heat': miamiHeat,
-    'Milwaukee Bucks': milwaukeeBucks,
-    'Minnesota Timberwolves': minnesotaTimberwolves,
-    'New Orleans Pelicans': newOrleansPelicans,
-    'New York Knicks': newYorkKnicks,
-    'Oklahoma City Thunder': oklahomaCityThunder,
-    'Orlando Magic': orlandoMagic,
-    'Philadelphia 76ers': philadelphia76ers,
-    'Phoenix Suns': phoenixSuns,
-    'Portland Trail Blazers': portlandTrailBlazers,
-    'Sacramento Kings': sacramentoKings,
-    'San Antonio Spurs': sanAntonioSpurs,
-    'Toronto Raptors': torontoRaptors,
-    'Utah Jazz': utahJazz,
-    'Washington Wizards': washingtonWizards,
-  }
 
   useEffect(() => {
     fetch('https://api.the-odds-api.com/v4/sports/basketball_nba/scores/?daysFrom=1&apiKey=f1e3276ebee2f8551da02b315a7a0d7e')
--- src/teamImages.jsx
+++ src/teamImages.jsx
@@ -1,35 +1,35 @@
-export { default as atlantaHawks } from '/NBA_Teams/Atlanta Hawks.svg';
-export { default as bostonCeltics } from '/NBA_Teams/Boston Celtics.svg';
-export { default as brooklynNets } from '/NBA_Teams/Brooklyn Nets.svg';
-export { default as charlotteHornets } from '/NBA_Teams/Charlotte Hornets.svg';
-export { default as chicagoBulls } from '/NBA_Teams/Chicago Bulls.svg';
-export { default as clevelandCavaliers } from '/NBA_Teams/Cleveland Cavaliers.svg';
-export { default as dallasMavericks } from '/NBA_Teams/Dallas Mavericks.svg';
-export { default as denverNuggets } from '/NBA_Teams/Denver Nuggets.svg';
-export { default as detroitPistons } from '/NBA_Teams/Detroit Pistons.svg';
-export { default as goldenStateWarriors } from '/NBA_Teams/Golden State Warriors.svg';
-export { default as houstonRockets } from '/NBA_Teams/Houston Rockets.svg';
-export { default as indianaPacers } from '/NBA_Teams/Indiana Pacers.svg';
-export { default as laClippers } from '/NBA_Teams/Los Angeles Clippers.svg';
-export { default as laLakers } from '/NBA_Teams/Los Angeles Lakers.svg';
-export { default as memphisGrizzlies } from '/NBA_Teams/Memphis Grizzlies.svg';
-export { default as miamiHeat } from '/NBA_Teams/Miami Heat.svg';
-export { default as milwaukeeBucks } from '/NBA_Teams/Milwaukee Bucks.svg';
-export { default as minnesotaTimberwolves } from '/NBA_Teams/Minnesota Timberwolves.svg';
-export { default as newOrleansPelicans } from '/NBA_Teams/New Orleans Pelicans.svg';
-export { default as newYorkKnicks } from '/NBA_Teams/New York Knicks.svg';
-export { default as oklahomaCityThunder } from '/NBA_Teams/Oklahoma City Thunder.svg';
-export { default as orlandoMagic } from '/NBA_Teams/Orlando Magic.svg';
-export { default as philadelphia76ers } from '/NBA_Teams/Philadelphia 76ers.svg';
-export { default as phoenixSuns } from '/NBA_Teams/Phoenix Suns.svg';
-export { default as portlandTrailBlazers } from '/NBA_Teams/Portland Trail Blazers.svg';
-export { default as sacramentoKings } from '/NBA_Teams/Sacramento Kings.svg';
-export { default as sanAntonioSpurs } from '/NBA_Teams/San Antonio Spurs.svg';
-export { default as torontoRaptors } from '/NBA_Teams/Toronto Raptors.svg';
-export { default as utahJazz } from '/NBA_Teams/Utah Jazz.svg';
-export { default as washingtonWizards } from '/NBA_Teams/Washington Wizards.svg';
+import atlantaHawks from '/NBA_Teams/Atlanta Hawks.svg';
+import bostonCeltics from '/NBA_Teams/Boston Celtics.svg';
+import brooklynNets from '/NBA_Teams/Brooklyn Nets.svg';
+import charlotteHornets from '/NBA_Teams/Charlotte Hornets.svg';
+import chicagoBulls from '/NBA_Teams/Chicago Bulls.svg';
+import clevelandCavaliers from '/NBA_Teams/Cleveland Cavaliers.svg';
+import dallasMavericks from '/NBA_Teams/Dallas Mavericks.svg';
+import denverNuggets from '/NBA_Teams/Denver Nuggets.svg';
+import detroitPistons from '/NBA_Teams/Detroit Pistons.svg';
+import goldenStateWarriors from '/NBA_Teams/Golden State Warriors.svg';
+import houstonRockets from '/NBA_Teams/Houston Rockets.svg';
+import indianaPacers from '/NBA_Teams/Indiana Pacers.svg';
+import laClippers from '/NBA_Teams/Los Angeles Clippers.svg';
+import laLakers from '/NBA_Teams/Los Angeles Lakers.svg';
+import memphisGrizzlies from '/NBA_Teams/Memphis Grizzlies.svg';
+import miamiHeat from '/NBA_Teams/Miami Heat.svg';
+import milwaukeeBucks from '/NBA_Teams/Milwaukee Bucks.svg';
+import minnesotaTimberwolves from '/NBA_Teams/Minnesota Timberwolves.svg';
+import newOrleansPelicans from '/NBA_Teams/New Orleans Pelicans.svg';
+import newYorkKnicks from '/NBA_Teams/New York Knicks.svg';
+import oklahomaCityThunder from '/NBA_Teams/Oklahoma City Thunder.svg';
+import orlandoMagic from '/NBA_Teams/Orlando Magic.svg';
+import philadelphia76ers from '/NBA_Teams/Philadelphia 76ers.svg';
+import phoenixSuns from '/NBA_Teams/Phoenix Suns.svg';
+import portlandTrailBlazers from '/NBA_Teams/Portland Trail Blazers.svg';
+import sacramentoKings from '/NBA_Teams/Sacramento Kings.svg';
+import sanAntonioSpurs from '/NBA_Teams/San Antonio Spurs.svg';
+import torontoRaptors from '/NBA_Teams/Toronto Raptors.svg';
+import utahJazz from '/NBA_Teams/Utah Jazz.svg';
+import washingtonWizards from '/NBA_Teams/Washington Wizards.svg';
 
-const teamImages = {
+export const teamImages = {
     'Atlanta Hawks': atlantaHawks,
     'Boston Celtics': bostonCeltics,
     'Brooklyn Nets': brooklynNets,
